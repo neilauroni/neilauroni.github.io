7d580ee7c2c9ae1701b902e5cb216a05260b86c1
neilauroni <68305760+neilauroni@users.noreply.github.com>
2022-11-04 13:50:08 -0400

    Fuck casey neistat

--- index.js
+++ index.js
@@ -1,34 +1,70 @@
-var replies = [
-    'g',
-    'neck',
-    'necktie',
-    'nectar',
-    'juj',
-    'vuv',
-    'VUVUVUV',
-    'SSSOTFJSDFMSMD',
-    'h',
-    'no',
-    'phelps',
-    'bro u ever jorps',
-    'jorps',
-    't',
-    'r',
-    'i',
-    'There are no more barriers to cross. All I have in common with the uncontrollable and the insane, the vicious and the evil, all the mayhem I have caused and my utter indifference toward it I have now surpassed. My pain is constant and sharp and I do not hope for a better world for anyone. In fact, I want my pain to be inflicted on others. I want no one to escape, but even after admitting this, there is no catharsis. My punishment continues to elude me and I gain no deeper knowledge of myself. No new knowledge can be extracted from my telling. This confession has meant nothing.',
-    'jester',
-    'JESTER',
-    'RR',
-    'E E',
-    'ZAZAZAJJJUJJJJJ',
-    '++++++++++',
-    'ZIZZZZZZ',
-    'ziss',
-    '++++***]*]*]*]*]*]*.!.?|¥\%{?!’klpwpwpw',
-    '?/?/?/???????/???/??????/????:?:?:?????/?/?',
-    'Z'
-];
+// var pic_array = [];
+// var i = 0;
 
-function buttonClick() {
-    document.getElementById("p1").innerHTML = replies[Math.floor(Math.random() * replies.length)]
-};
+// var roll_num = 1;
+
+// document.getElementById("counter");
+
+// function setImage() {
+
+//     for (var i = 1; i <= length; i++) {
+//         pic_array.push(i);
+//     }
+
+    
+// }
+
+// function changeRoll(num) {
+//     roll_num = num;
+//     img.src = "images/roll " + roll_num + "/image" + pic_array[1] + ".JPG";
+
+// }
+
+var pic_array = [1, 2, 3];
+var i = 0;
+var roll_num = 1;
+
+function setCounter() {
+    document.getElementById("counter").textContent = pic_array[i] + "/" + pic_array[pic_array.length - 1];
+}
+
+
+
+function changeRoll (num) {
+    roll_num = num;
+    if (roll_num == 1) {
+        pic_array = [1, 2, 3];
+    }
+
+    if (roll_num == 2) {
+        pic_array = [1]
+    }
+    img.src = "images/roll " + roll_num + "/image" + pic_array[1] + ".JPG";
+
+
+}
+
+function leftClick() {
+    var img = document.getElementById("current-pic");
+    i--;
+    if (i < 0) {
+        i = pic_array.length - 1;
+    }
+    img.src = "images/roll " + roll_num + "/image" + pic_array[i] + ".JPG";
+    document.getElementById("counter").textContent = pic_array[i] + "/" + pic_array[pic_array.length - 1]
+1
+
+    
+}
+
+function rightClick() {
+    var img = document.getElementById("current-pic");
+    i++;
+    if (i >= pic_array.length) {
+        i = 0;
+    }
+    img.src = "images/roll " + roll_num + "/image" + pic_array[i] + ".JPG";
+    document.getElementById("counter").textContent = pic_array[i] + "/" + pic_array[pic_array.length - 1]
+
+    
+}
